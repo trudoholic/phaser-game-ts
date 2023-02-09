05ef3dfdefacb7584db922b79fb8194e4af69558
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-02-09 20:45:31 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import phaserGame from './PhaserGame'
+import HelloWorldScene from './scenes/HelloWorldScene'
+
+const handleClick = () => {
+    const scene = phaserGame.scene.keys.helloworld as HelloWorldScene
+    scene.createEmitter()
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button className="App-button" onClick={handleClick}>
+              Or click me
+          </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
